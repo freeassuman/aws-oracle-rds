45ec0e37ae5e7f404bd89c86d743b64f61c20b42
Suman Das <suman5848@gmail.com>
2025-08-28 15:52:36 +0000

    changed s3

--- environments/test/backend.tf
+++ environments/test/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.11"
 
   backend "s3" {
-    bucket       = "suman-tf-state-test"
+    bucket       = "bat2nlblogs"
     key          = "oracle-rds/terraform.tfstate"
     region       = "eu-west-1"
     encrypt      = true
