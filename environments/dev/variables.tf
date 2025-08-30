009fa3658022f73ab8283ce909b6e21f5a0e0add
Suman Das <suman5848@gmail.com>
2025-08-30 14:24:47 +0000

    add prod

---
+++ environments/dev/variables.tf
@@ -0,0 +1,77 @@
+variable "name_prefix" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "allowed_cidr_blocks" {
+  type    = list(string)
+  default = []
+}
+
+variable "engine_edition" {
+  type = string
+}
+
+variable "engine_version" {
+  type = string
+}
+
+variable "instance_class" {
+  type = string
+}
+
+variable "multi_az" {
+  type    = bool
+  default = false
+}
+
+variable "username" {
+  type = string
+}
+
+variable "password" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_port" {
+  type    = number
+  default = 1521
+}
+
+variable "db_parameters" {
+  type = list(object({
+    name  = string
+    value = string
+  }))
+  default = []
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "db_options" {
+  type = list(object({
+    option_name                    = string
+    port                           = optional(number)
+    version                        = optional(string)
+    persistent                     = optional(bool)
+    permanent                      = optional(bool)
+    vpc_security_group_memberships = optional(list(string))
+    option_settings = optional(list(object({
+      name  = string
+      value = string
+    })))
+  }))
+  default = []
+}
---
+++ environments/prod/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.11"
+  backend "s3" {
+    bucket       = "suman-tf-state-prod"
+    key          = "oracle-rds/terraform.tfstate"
+    region       = "eu-west-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
